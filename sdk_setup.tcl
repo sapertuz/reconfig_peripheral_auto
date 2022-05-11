da4c14a440a9076bfb05105cb112fbb352675ac2
Sergio Pertuz <sergio.pertuz@tu-dresden.de>
2022-05-11 12:43:03 +0200

    fix sdk TCL

--- sdk_setup.tcl
+++ sdk_setup.tcl
@@ -30,14 +30,15 @@ platform generate
 
 # 10-3. Create Application
     #createapp -name $app_name -app {Empty Application} -bsp $bsp_fat -hwproject hw -proc ps7_cortexa9_0
-app create -name $app_name -template "Empty Application(C)" -platform "hw" -os standalone                                                                                  
+app create -name $app_name -template "Empty Application(C)" -platform "hw" -proc ps7_cortexa9_0 -os standalone
+
 importsources -name $app_name -path $testapp_dir
     #configapp -app $app_name define-compiler-symbols $BOARD_GLOBAL
 app config -name $app_name define-compiler-symbols $BOARD_GLOBAL
 
 # 10-4. Create a zynq_fsbl application.
     #createapp -name fsbl -app {Zynq FSBL} -proc ps7_cortexa9_0 -hwproject hw -os standalone
-app create -name fsbl -app "Zynq FSBL" -proc ps7_cortexa9_0 -platform "hw" -os standalone
+app create -name fsbl -template "Zynq FSBL" -proc ps7_cortexa9_0 -platform "hw" -os standalone
 
 ## Build all projects
     #projects -build
